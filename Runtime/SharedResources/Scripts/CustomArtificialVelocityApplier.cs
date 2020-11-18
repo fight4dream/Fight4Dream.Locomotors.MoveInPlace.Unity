03c9f02fd93d51eb14874a212b87bcdcbfa7da0a
Ethan Cheung <ethan@fight4dream.com>
2020-11-18 14:06:43 +0800

    -> attempt to fix an issue that reported it moves slower in quest sus frame rate is lower on quest

---
+++ Runtime/SharedResources/Scripts/CustomArtificialVelocityApplier.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Zinnia.Extension;
+using Zinnia.Process;
+using Zinnia.Tracking.Velocity;
+
+namespace Fight4Dream.Locomotors.MoveInPlace.Unity
+{
+    public class CustomArtificialVelocityApplier : ArtificialVelocityApplier, IProcessable
+    {
+        protected bool canProcess { get; set; }
+
+        public override void Apply()
+        {
+            canProcess = true;
+        }
+
+        public void Process()
+        {
+            if (!canProcess)
+            {
+                return;
+            }
+
+            if (!Velocity.ApproxEquals(Vector3.zero, NilVelocityTolerance) || !AngularVelocity.ApproxEquals(Vector3.zero, NilAngularVelocityTolerance))
+            {
+                float deltaTime = Time.inFixedTimeStep ? Time.fixedDeltaTime : Time.deltaTime;
+                Velocity = Vector3.Slerp(Velocity, Vector3.zero, Drag * deltaTime);
+                AngularVelocity = Vector3.Slerp(AngularVelocity, Vector3.zero, AngularDrag * deltaTime);
+                Target.transform.localRotation *= Quaternion.Euler(AngularVelocity);
+                Target.transform.localPosition += Velocity * Time.deltaTime;
+            }
+            else
+            {
+                Velocity = Vector3.zero;
+                AngularVelocity = Vector3.zero;
+                canProcess = false;
+            }
+        }
+
+        protected override void CancelDeceleration()
+        {
+            canProcess = false;
+        }
+    }
+}
