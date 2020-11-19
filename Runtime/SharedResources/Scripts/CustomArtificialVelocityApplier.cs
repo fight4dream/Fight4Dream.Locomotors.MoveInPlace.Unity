4ebbe348f34417eb29cdf690939bd9dd3b746047
Ethan Cheung <ethan@fight4dream.com>
2020-11-19 18:50:52 +0800

    -> derived from AverageVelocityApplier to implement IProcessable so that we can have deceleration at different moment -> change to linear drag (not slerp) -> added IncrementVelocity and IncrementAngularVelocity to simplify steps - removed auxillary scripts ArtificialVelocityExtractor and Vector3Adder due to above simplifications

--- Runtime/SharedResources/Scripts/CustomArtificialVelocityApplier.cs
+++ Runtime/SharedResources/Scripts/CustomArtificialVelocityApplier.cs
@@ -7,6 +7,16 @@ namespace Fight4Dream.Locomotors.MoveInPlace.Unity
 {
     public class CustomArtificialVelocityApplier : ArtificialVelocityApplier, IProcessable
     {
+        public void IncrementVelocity(Vector3 amount)
+        {
+            Velocity += amount;
+        }
+
+        public void IncrementAngularVelocity(Vector3 amount)
+        {
+            AngularVelocity += amount;
+        }
+
         protected bool canProcess { get; set; }
 
         public override void Apply()
@@ -24,8 +34,8 @@ namespace Fight4Dream.Locomotors.MoveInPlace.Unity
             if (!Velocity.ApproxEquals(Vector3.zero, NilVelocityTolerance) || !AngularVelocity.ApproxEquals(Vector3.zero, NilAngularVelocityTolerance))
             {
                 float deltaTime = Time.inFixedTimeStep ? Time.fixedDeltaTime : Time.deltaTime;
-                Velocity = Vector3.Slerp(Velocity, Vector3.zero, Drag * deltaTime);
-                AngularVelocity = Vector3.Slerp(AngularVelocity, Vector3.zero, AngularDrag * deltaTime);
+                Velocity = Vector3.Lerp(Velocity, Vector3.zero, Drag * deltaTime);
+                AngularVelocity = Vector3.Lerp(AngularVelocity, Vector3.zero, AngularDrag * deltaTime);
                 Target.transform.localRotation *= Quaternion.Euler(AngularVelocity);
                 Target.transform.localPosition += Velocity * Time.deltaTime;
             }
